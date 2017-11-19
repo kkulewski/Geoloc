95a2537cd2f5b25d4a46d54f37faf6e0d8fb2450
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-20 00:53:06 +0100

    Add location controller

---
+++ Geoloc/Controllers/LocationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Geoloc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Geoloc.Controllers
+{
+    public static class LocationRepository
+    {
+        public static IList<LocationModel> Locations = new List<LocationModel>();
+    }
+
+    [Route("api/[controller]")]
+    public class LocationController : Controller
+    {
+        [HttpPost("[action]")]
+        public void Send([FromBody]LocationModel model)
+        {
+            LocationRepository.Locations.Add(model);
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<LocationModel> Get()
+        {
+            return LocationRepository.Locations;
+        }
+
+    }
+}
