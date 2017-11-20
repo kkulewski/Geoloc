50d9eb1f563de9e8d110cdbe993c0a109c454631
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-20 01:18:10 +0100

    Add location repo interface

---
+++ Geoloc/Repository/ILocationRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Geoloc.Models;
+
+namespace Geoloc.Repository
+{
+    public interface ILocationRepository
+    {
+        void Add(LocationModel model);
+
+        IEnumerable<LocationModel> Get();
+    }
+}
