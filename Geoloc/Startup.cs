e35857b30d675e1e7668f65c6e4dc57c5ef37db5
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-21 23:53:10 +0100

    Add db context

--- Geoloc/Startup.cs
+++ Geoloc/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Geoloc.Data;
 using Geoloc.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -25,6 +27,10 @@ namespace Geoloc
         {
             services.AddMvc();
             services.AddSingleton<ILocationRepository, InMemoryLocationRepository>();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("Geoloc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
