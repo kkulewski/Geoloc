94ba4db66b8a30b9520c456971ee242df318a31c
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-22 02:19:38 +0100

    Add register vm + mapping

---
+++ Geoloc/ViewModels/Mappings/ViewModelToEntityMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Geoloc.Models.Entities;
+
+namespace Geoloc.Models.ViewModels.Mappings
+{
+    public class ViewModelToEntityMappingProfile : Profile
+    {
+        public ViewModelToEntityMappingProfile()
+        {
+            CreateMap<RegisterViewModel, AppUser>()
+                .ForMember(au => au.UserName, map => map.MapFrom(vm => vm.Email));
+        }
+    }
+}
