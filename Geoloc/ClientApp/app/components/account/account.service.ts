29ec0b4caf53cb8eeb703770b99636b4b3c603f0
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-25 00:28:58 +0100

    Extract account service

---
+++ Geoloc/ClientApp/app/components/account/account.service.ts
@@ -0,0 +1,14 @@
+import { Http, Headers, RequestOptions } from '@angular/http';
+
+export class AccountService {
+
+  constructor(private http: Http, private baseUrl: string) {}
+
+  register(email: string, password: string, firstName: string, lastName: string) {
+    let body = JSON.stringify({ email, password, firstName, lastName });
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.baseUrl + "api/account/register", body, options);
+  }
+}
--- Geoloc/ClientApp/app/components/account/register/register.component.ts
+++ Geoloc/ClientApp/app/components/account/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { IRegisterModel } from '../models/register.model';
+import { AccountService } from '../account.service';
 
 @Component({
     selector: 'register',
@@ -10,13 +11,16 @@ import { IRegisterModel } from '../models/register.model';
 export class RegisterComponent {
     isRequesting: boolean = false;
     resultMessage = "";
+    accountService: AccountService;
 
-    constructor(private router: Router, private http: Http, @Inject("BASE_URL") private baseUrl: string) {}
+    constructor(private router: Router, private http: Http, @Inject("BASE_URL") private baseUrl: string) { 
+      this.accountService = new AccountService(this.http, this.baseUrl)
+  }
 
     registerUser({ value, valid }: { value: IRegisterModel, valid: boolean }) {
         this.isRequesting = true;
         if (valid) {
-            this.register(value.email, value.password, value.firstName, value.lastName)
+            this.accountService.register(value.email, value.password, value.firstName, value.lastName)
                 .subscribe(
                     (response: Response) => {
                         this.resultMessage = response.text();
@@ -27,13 +31,5 @@ export class RegisterComponent {
                         this.isRequesting = false;
                     });
         }
-    }
-
-    register(email: string, password: string, firstName: string, lastName: string) {
-        let body = JSON.stringify({ email, password, firstName, lastName });
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
-
-        return this.http.post(this.baseUrl + "api/account/register", body, options);
     }
 }
