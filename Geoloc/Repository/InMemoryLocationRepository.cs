ad3f4b8a664425b448be564c1e53eff48a5fb92c
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-26 23:14:05 +0100

    Restore Repository

---
+++ Geoloc/Repository/InMemoryLocationRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Geoloc.Models;
+
+namespace Geoloc.Repository
+{
+    public class InMemoryLocationRepository : ILocationRepository
+    {
+        private static readonly IList<LocationModel> Locations = new List<LocationModel>();
+
+        public void Add(LocationModel model)
+        {
+            Locations.Add(model);
+        }
+
+        public IEnumerable<LocationModel> Get()
+        {
+            return Locations;
+        }
+    }
+}
