62f664550e014ad56fd41c914691b059b43f46f2
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-26 23:13:51 +0100

    Restore Models

---
+++ Geoloc/Models/LocationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Geoloc.Models
+{
+    public class LocationModel
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string UserName { get; set; }
+        public long Timestamp { get; set; }
+    }
+}
