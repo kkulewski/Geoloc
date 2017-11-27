7b86ff3f3ea05154d89ea9f01732ce5b4745992e
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-11-28 00:39:31 +0100

    Init in-memory location repo with example data

--- Geoloc/Repository/InMemoryLocationRepository.cs
+++ Geoloc/Repository/InMemoryLocationRepository.cs
@@ -5,7 +5,15 @@ namespace Geoloc.Repository
 {
     public class InMemoryLocationRepository : ILocationRepository
     {
-        private static readonly IList<LocationModel> Locations = new List<LocationModel>();
+        private static readonly IList<LocationModel> Locations = new List<LocationModel>
+        {
+            new LocationModel { Latitude = 53.101, Longitude = 18.505, Timestamp = 1511825100800, UserName = "john@test.com" },
+            new LocationModel { Latitude = 53.122, Longitude = 18.515, Timestamp = 1511825105800, UserName = "john@test.com" },
+            new LocationModel { Latitude = 51.005, Longitude = 17.805, Timestamp = 1511825105850, UserName = "anne@test.com" },
+            new LocationModel { Latitude = 53.101, Longitude = 18.505, Timestamp = 1511825112900, UserName = "john@test.com" },
+            new LocationModel { Latitude = 52.335, Longitude = 18.115, Timestamp = 1511825112950, UserName = "dave@test.com" },
+            new LocationModel { Latitude = 53.125, Longitude = 18.525, Timestamp = 1511825121200, UserName = "john@test.com" }
+        };
 
         public void Add(LocationModel model)
         {
