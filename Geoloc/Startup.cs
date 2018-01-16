fba50291770289780082e0f64e144aaa845ec710
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-01-17 01:36:40 +0200

    Loosen user password requirements

--- Geoloc/Startup.cs
+++ Geoloc/Startup.cs
@@ -40,6 +40,15 @@ namespace Geoloc
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 3;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+            });
+
             services.AddAuthentication(options =>
                 {
                     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
