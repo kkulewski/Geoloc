6119bd349c4e8746be6cdb2f4d667d3d8af62b47
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-10 22:35:57 +0100

    Add sent/received relations action to controller

--- Geoloc/Controllers/UserRelationController.cs
+++ Geoloc/Controllers/UserRelationController.cs
@@ -20,10 +20,26 @@ namespace Geoloc.Controllers
             _userRelationRepository = userRelationRepository;
         }
 
-        [HttpGet("[action]/{userId}")]
-        public IEnumerable<UserRelationWebModel> Get(Guid userId)
+        [HttpGet("{userId}")]
+        public IEnumerable<UserRelationWebModel> Relations(Guid userId)
         {
             var model = _userRelationRepository.GetUserRelations(userId);
+            var result = Mapper.Map<IEnumerable<UserRelationWebModel>>(model);
+            return result;
+        }
+
+        [HttpGet("sent/{userId}")]
+        public IEnumerable<UserRelationWebModel> GetSentRequests(Guid userId)
+        {
+            var model = _userRelationRepository.GetUserSentRequests(userId);
+            var result = Mapper.Map<IEnumerable<UserRelationWebModel>>(model);
+            return result;
+        }
+
+        [HttpGet("received/{userId}")]
+        public IEnumerable<UserRelationWebModel> GetReceivedRelations(Guid userId)
+        {
+            var model = _userRelationRepository.GetUserReceivedRequests(userId);
             var result = Mapper.Map<IEnumerable<UserRelationWebModel>>(model);
             return result;
         }
