08e54facc2caf75853fd201bdd986ee9901c274b
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-25 18:49:28 +0200

    Add meeting migration

--- Geoloc/Migrations/20180325164852_RenameEventToMeeting.Designer.cs
+++ Geoloc/Migrations/20180325164852_RenameEventToMeeting.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Geoloc.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180325164852_RenameEventToMeeting")]
+    partial class RenameEventToMeeting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
