a9b38b71352528b0f948e152fac83bf46bf94249
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-25 19:04:42 +0200

    Add meeting add action

--- Geoloc/Controllers/MeetingController.cs
+++ Geoloc/Controllers/MeetingController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using AutoMapper;
+using Geoloc.Models;
 using Geoloc.Models.WebModels;
 using Geoloc.Services.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Geoloc.Controllers
 {
@@ -22,6 +24,30 @@ namespace Geoloc.Controllers
             var model = _meetingService.GetById(meetingId);
             var result = Mapper.Map<MeetingWebModel>(model);
             return result;
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Add([FromBody]MeetingWebModel webModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var model = new MeetingModel
+            {
+                Name = webModel.Name,
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now + TimeSpan.FromHours(1)
+            };
+
+            var isSuccess = _meetingService.AddMeeting(model);
+            if (!isSuccess)
+            {
+                return BadRequest();
+            }
+
+            return new OkObjectResult(JsonConvert.SerializeObject("Meeting added"));
         }
     }
 }
