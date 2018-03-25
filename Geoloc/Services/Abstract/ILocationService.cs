0d839e3c02002a3742d37afaddb43451c97debce
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-03-25 20:47:13 +0200

    Add get by id to location service and repo

--- Geoloc/Services/Abstract/ILocationService.cs
+++ Geoloc/Services/Abstract/ILocationService.cs
@@ -6,6 +6,7 @@ namespace Geoloc.Services.Abstract
 {
     public interface ILocationService
     {
+        LocationModel GetById(Guid locationId);
         bool AddLocation(LocationModel model);
         IEnumerable<LocationModel> GetLocationByUserId(Guid userId);
         IEnumerable<LocationModel> GetLastKnownLocations();
