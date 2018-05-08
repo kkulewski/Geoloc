0349d057189c6d1f1149b13325789d00b4f1f511
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-05-09 01:34:43 +0200

    Add UserRelation to Relation rename migration

--- Geoloc/Migrations/20180508233215_RenameUserRelationToRelation.Designer.cs
+++ Geoloc/Migrations/20180508233215_RenameUserRelationToRelation.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Geoloc.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180508233215_RenameUserRelationToRelation")]
+    partial class RenameUserRelationToRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
